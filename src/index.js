6b59b11cb3f10a54798e5d3e6ce0354d47cedb75
Tadeu Saldanha <tadeusaldanha@gmail.com>
2018-04-29 19:40:43 +0100

    Webpack config + Mapbox render

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import mapboxgl from 'mapbox-gl';
+import './style.css';
+
+
+(function(){
+
+	mapboxgl.accessToken = 'pk.eyJ1IjoidHNhbGRhbmhhIiwiYSI6ImNqZ2p4cDhqZjFrOGkyd3FvaXVzdmN2MHMifQ.e0i8Mrl2Z738v3FPQsH_0w';
+	var map = new mapboxgl.Map({
+		container: 'map',
+		style: 'mapbox://styles/mapbox/streets-v10',
+		center: [-9.17,38.74], // starting position [lng, lat]
+    	zoom: 12 // starting zoom
+	});	
+})();
---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+	mode: 'development',
+	entry: './src/index.js',
+	output: {
+		filename: 'app.js',
+		path: path.resolve(__dirname, 'dist')
+	},
+  	devtool: 'inline-source-map',
+	devServer: {
+		contentBase: './dist'
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+	   		title: 'Lisbon Cycle',
+	   		template: 'src/index.html'
+		}),
+		new CleanWebpackPlugin(['dist'])
+	],
+	module: {
+		rules: [
+			{
+				test: /\.css$/,
+				use: [
+			   		'style-loader',
+			   		'css-loader'
+			 	]
+			},
+			{
+				test: /\.(png|svg|jpg|gif)$/,
+				use: [
+					'file-loader'
+				]
+			},
+			{
+		         test: /\.(woff|woff2|eot|ttf|otf)$/,
+		         use: [
+		           'file-loader'
+		         ]
+		    },
+		    {
+		         test: /\.(csv|tsv)$/,
+		         use: [
+		           'csv-loader'
+		         ]
+	       },
+	       {
+		         test: /\.xml$/,
+		         use: [
+		           'xml-loader'
+		         ]
+	       }
+		]
+	}
+};
