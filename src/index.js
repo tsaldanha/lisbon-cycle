21f63638698343a9ecdc2e700436799b7558a359
Tadeu Saldanha <tadeusaldanha@gmail.com>
2018-04-29 20:57:25 +0100

    Lisbon bike layer working

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import mapboxgl from 'mapbox-gl';
 import './style.css';
+import Data from './lisbon.json'
 
 
 (function(){
@@ -7,8 +8,30 @@ import './style.css';
 	mapboxgl.accessToken = 'pk.eyJ1IjoidHNhbGRhbmhhIiwiYSI6ImNqZ2p4cDhqZjFrOGkyd3FvaXVzdmN2MHMifQ.e0i8Mrl2Z738v3FPQsH_0w';
 	var map = new mapboxgl.Map({
 		container: 'map',
-		style: 'mapbox://styles/mapbox/streets-v10',
+		style: 'mapbox://styles/tsaldanha/cjgl7uxqe000i2sqive3fsfsm',
 		center: [-9.17,38.74], // starting position [lng, lat]
     	zoom: 12 // starting zoom
 	});	
+
+	map.on('load', function() {
+		map.addLayer({
+        "id": "route",
+        "type": "line",
+        "source": {
+            "type": "geojson",
+            "data": Data
+        },
+        "layout": {
+            "line-join": "round",
+            "line-cap": "round"
+        },
+        "paint": {
+            "line-color": "#888",
+            "line-width": 2
+        }
+    });
+
+	});
+	
+
 })();
