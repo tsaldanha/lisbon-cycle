1c4501aa689421aa7b02a650e16feaa590df8e12
Tadeu Saldanha <tadeusaldanha@gmail.com>
2018-04-29 21:41:02 +0100

    production settings

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,6 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
-	mode: 'development',
 	entry: './src/index.js',
 	output: {
 		filename: 'app.js',
